0af12a5593dc0012f4701673a51aaee74e0fb488
Bart Jansen <b@rtjansen.nl>
2016-07-01 09:42:22 +0200

    Added RTT and PL to charts

--- app/charts.js
+++ app/charts.js
@@ -19,17 +19,15 @@ document.addEventListener("DOMContentLoaded", function(event) {
     firebase.initializeApp(config);
 
     firebase.database().ref('logs').once('value', function(snapshot) {
+
+
         window.fbData = snapshot.val();
         var val = window.fbData;
-
-
-
 
         for(var key in val) {
             if(val[key]) {
                 selectObj.options[selectObj.options.length] = new Option(val[key].fileName + ' ('+val[key].callLength+' seconds)', key);
             }
-
         }
 
 
@@ -39,7 +37,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
 function updateCharts(data) {
 
-    var totRecvBw = [],totSentBw = [], sentFps = [], recvFps = [], sentRes = [], recvRes = [], xAxis = [];
+    var RTT, totRecvBw = [],totSentBw = [], sentFps = [], recvFps = [], sentRes = [], recvRes = [], totPL = [], totRTT = [], xAxis = [];
 
     for(var i = 0; i < data.length; i++) {
         xAxis.push(i);
@@ -55,7 +53,13 @@ function updateCharts(data) {
 
         sentRes.push(data[i].sentVideo.googFrameWidthSent || '0');
         recvRes.push(data[i].recvVideo.googFrameWidthReceived || '0');
+
+        var RTT = (parseInt(data[i].sentAudio.googRtt) + parseInt(data[i].sentVideo.googRtt)) / 2;
+
+        totRTT.push(RTT > 0 ? RTT : 0);
+        totPL.push(getPL(data[i], i !== 0 ? data[i-1] : ''));
     }
+
 
 
     window.myBwChart.data.labels = xAxis;
@@ -74,6 +78,14 @@ function updateCharts(data) {
     window.myResChart.data.datasets[0].data = sentRes;
     window.myResChart.data.datasets[1].data = recvRes;
     window.myResChart.update();
+
+    window.myPlChart.data.labels = xAxis;
+    window.myPlChart.data.datasets[0].data = totPL;
+    window.myPlChart.update();
+
+    window.myRttChart.data.labels = xAxis;
+    window.myRttChart.data.datasets[0].data = totRTT;
+    window.myRttChart.update();
 }
 
 
@@ -176,6 +188,78 @@ function createCharts() {
             }
         }
     });
+
+    var plChart = document.getElementById("plChart");
+    window.myPlChart = new Chart(plChart, {
+        type: 'line',
+        data: {
+            labels: [],
+            datasets: [{
+                label: 'Packet loss',
+                data: [],
+                backgroundColor: 'rgba(255, 99, 132, 0)',
+                borderColor: 'rgba(255,99,132,1)',
+                borderWidth: 1
+            }]
+        },
+        options: {
+            responsive:true,
+            maintainAspectRatio: false,
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero:true
+                    }
+                }]
+            }
+        }
+    });
+
+    var rttChart = document.getElementById("rttChart");
+    window.myRttChart = new Chart(rttChart, {
+        type: 'line',
+        data: {
+            labels: [],
+            datasets: [{
+                label: 'RTT',
+                data: [],
+                backgroundColor: 'rgba(255, 99, 132, 0)',
+                borderColor: 'rgba(255,99,132,1)',
+                borderWidth: 1
+            }]
+        },
+        options: {
+            responsive:true,
+            maintainAspectRatio: false,
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero:true
+                    }
+                }]
+            }
+        }
+    });
+}
+
+function getPL(oldVal, newVal) {
+    var loss, result = [];
+
+    loss = 0;
+
+    if(oldVal && newVal['recvAudio'] && newVal['recvVideo'] && newVal['recvAudio'].packetsReceived && newVal['recvVideo'].packetsReceived &&
+        oldVal['recvAudio'] && oldVal['recvVideo'] && oldVal['recvAudio'].packetsReceived && oldVal['recvVideo'].packetsReceived) {
+
+        loss = ((((newVal['recvAudio'].packetsLost-oldVal['recvAudio'].packetsLost) + (newVal['recvVideo'].packetsLost-oldVal['recvVideo'].packetsLost)) /
+            (((newVal['recvAudio'].packetsReceived-oldVal['recvAudio'].packetsReceived) + (newVal['recvVideo'].packetsReceived-oldVal['recvVideo'].packetsReceived)) +
+            ((newVal['recvAudio'].packetsLost-oldVal['recvAudio'].packetsLost) + (newVal['recvVideo'].packetsLost-oldVal['recvVideo'].packetsLost)))) * 100).toFixed(2);
+
+    }
+    if(isNaN(loss))
+        loss = 0;
+    // console.log(isNaN(loss))
+
+    return loss;
 }
 
 
